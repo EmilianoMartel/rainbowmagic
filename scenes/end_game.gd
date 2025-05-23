061f17c3500f9de6053ab495b8051b86e87a09e9
emiliano martel <emiliano.d.martel.r@gmail.com>
2025-05-23 20:46:50 -0300

    reset endgame

--- scenes/end_game.gd
+++ scenes/end_game.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
-@export var other_scene : PackedScene
+@export var final_scene : PackedScene
 @export var Success :  Texture
 @export var Fail : Texture
 @export var WinOrLose : Sprite2D
 
 func _ready():
+	check_win_or_lose()
 	pass
 
-func _on_play_button_button_down() -> void:
-	get_tree().change_scene_to_packed(other_scene)
+func _on_back_button_up() -> void:
+	get_tree().change_scene_to_packed(final_scene)
 	pass
 
 func check_win_or_lose():
